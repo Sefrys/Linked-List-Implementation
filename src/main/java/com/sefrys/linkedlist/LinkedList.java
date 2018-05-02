9254adbed12231126a745b82ceb0eba70176494d
Adonalsium <sefrys@gmail.com>
2018-05-02 17:01:56 +0200

    Add first link removal

--- src/main/java/com/sefrys/linkedlist/LinkedList.java
+++ src/main/java/com/sefrys/linkedlist/LinkedList.java
@@ -20,4 +20,31 @@ public class LinkedList {
         return (firstLink == null);
     }
 
+    /**
+     * Creating a new link, and setting it as first in list.
+     */
+    public void insertFirstLink(String address, String owner) {
+       Link newLink = new Link(address, owner);
+
+       newLink.next = firstLink;
+
+       firstLink = newLink;
+    }
+
+    /**
+     * Removing the first link and assigning its next link as the first link
+     * @return reference to new first link
+     */
+    public Link removeFirstLink() {
+        Link linkReference = firstLink;
+
+        if(!isEmpty()) {
+            firstLink = firstLink.next;
+        } else {
+            System.out.println("Empty linked list");
+        }
+
+        return linkReference;
+    }
+
 }
