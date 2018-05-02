a95e0f25864170e17a3200d2e6dfce69ac92a446
Adonalsium <sefrys@gmail.com>
2018-05-02 16:55:31 +0200

    Add linkedlist class

---
+++ src/main/java/com/sefrys/linkedlist/LinkedList.java
@@ -0,0 +1,15 @@
+package com.sefrys.linkedlist;
+
+/**
+ * Created by Ireneusz Zagan on 02.05.18, 16:43
+ * Contact: sefrys@gmail.com
+ */
+public class LinkedList {
+
+    public Link firstLink;
+
+    LinkedList() {
+        firstLink = null;
+    }
+
+}
