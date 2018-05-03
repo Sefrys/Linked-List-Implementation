5d9e2af7a41050a6bc87dceb77b2189f8ff9504e
Adonalsium <sefrys@gmail.com>
2018-05-03 16:47:35 +0200

    Add attribute 'previous' for DL list usage

--- src/main/java/com/sefrys/linkedlist/Link.java
+++ src/main/java/com/sefrys/linkedlist/Link.java
@@ -4,19 +4,25 @@ package com.sefrys.linkedlist;
  * Created by Ireneusz Zagan on 02.05.18, 16:45
  * Contact: sefrys@gmail.com
  */
-public class Link {
-    public String address;
-    public String owner;
+class Link {
+    String address;
+    String owner;
 
-    public Link next;
+    Link next;
+    Link previous;
 
-    public Link(String address, String owner) {
+
+    Link(String address, String owner) {
 
         this.address = address;
         this.owner = owner;
     }
 
-    public void display() {
+    void display() {
         System.out.println("Address and owner: " + address + " " + owner);
+    }
+
+    public String toString() {
+        return owner + " " + address;
     }
 }
